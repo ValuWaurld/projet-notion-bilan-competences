d905395813901fc6fa7b3172b7b5f9fda4590b5d
Khaled-AL-MUGHRABI <55314916+Khaled-AL-MUGHRABI@users.noreply.github.com>
2023-12-06 16:52:46 +0100

    Full_screen

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -1,5 +1,3 @@
-import 'phaser';
-
 export class PreloadScene extends Phaser.Scene {
     constructor() {
         super({ key: 'PreloadScene' });
@@ -11,7 +9,13 @@ export class PreloadScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(300, 300, 'loading');
+        // Obtenir les dimensions de la caméra
+        const width = this.cameras.main.width;
+        const height = this.cameras.main.height;
+
+        // Centrer l'image 'loading' sur la scène
+        this.add.image(width / 2, height / 2, 'loading');
+
         setTimeout(() => {
             this.scene.start('TitleScene');
         }, 2000);
--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -1,19 +1,28 @@
 import 'phaser';
-import { Align } from '../utils/Align';
 
 export class TitleScene extends Phaser.Scene {
-
     constructor() {
         super({ key: 'TitleScene' });
     }
 
     preload() {
-
+        // Assurez-vous que l'image 'forest' est chargée ici si ce n'est pas déjà fait
     }
 
     create() {
-        const forest = this.add.image(0, 0, 'forest');
-        Align.scaleToGameW(forest, 1);
-    }
+        const forest = this.add.image(0, 0, 'forest').setOrigin(0, 0);
 
+        const scaleX = this.cameras.main.width / forest.width;
+        const scaleY = this.cameras.main.height / forest.height;
+        const scale = Math.max(scaleX, scaleY);
+
+        forest.setScale(scale);
+
+        // Centrer l'image si nécessaire
+        if (scale === scaleX) {
+            forest.y = (this.cameras.main.height - forest.height * scale) / 2;
+        } else {
+            forest.x = (this.cameras.main.width - forest.width * scale) / 2;
+        }
+    }
 }
