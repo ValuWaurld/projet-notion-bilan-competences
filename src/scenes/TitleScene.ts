149e15e0c584a56333507ef999304f993756f0a3
Khaled-AL-MUGHRABI <55314916+Khaled-AL-MUGHRABI@users.noreply.github.com>
2023-12-18 16:55:40 +0100

    merge

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -14,17 +14,16 @@ export class TitleScene extends BaseScene {
     create() {
         const backgroundImage = this.add.image(0, 0, "OfficeBackground").setOrigin(0, 0);
         const scaleY = this.cameras.main.height / backgroundImage.height;
+        const scaleX = this.cameras.main.width / backgroundImage.width;
 
-        backgroundImage.setScale(1, scaleY);
+        backgroundImage.setScale(scaleX, scaleY);
         backgroundImage.preFX?.addBlur(0);
         backgroundImage.postFX?.addBlur(0);
         
-        // Center background if needed
         if (backgroundImage.width < this.cameras.main.width) {
             backgroundImage.setX((this.cameras.main.width - backgroundImage.width) / 2);
         }
 
-        // Add title text
         const titleText = this.add.text(this.cameras.main.centerX, 100, "MY GAME", {
             fontSize: "64px",
             color: "#000000",
@@ -38,19 +37,24 @@ export class TitleScene extends BaseScene {
         });
         titleText.setOrigin(0.5, 0.5);
 
-        const playButton = this.add.sprite(this.cameras.main.centerX, 300, "playButton").setInteractive();
-        playButton.setOrigin(0.5, 0.5);
-        playButton.on("pointerdown", () => {
+        // Bouton 'Play'
+        const playButton = this.add.text(this.cameras.main.centerX, 300, 'Play', { font: '32px Arial', color: '#000000' }) // Couleur modifiée en noir
+            .setInteractive()
+            .setOrigin(0.5, 0.5);
+
+        playButton.on('pointerdown', () => {
             this.scene.start('GameScene');
         });
 
-        const creditsButton = this.add.sprite(this.cameras.main.centerX, 500, "creditsButton").setInteractive();
-        creditsButton.setOrigin(0.5, 0.5);
-        creditsButton.on("pointerdown", () => {
+        // Bouton 'Credits'
+        const creditsButton = this.add.text(this.cameras.main.centerX, 400, 'Credits', { font: '32px Arial', color: '#000000' }) // Couleur modifiée en noir
+            .setInteractive()
+            .setOrigin(0.5, 0.5);
+
+        creditsButton.on('pointerdown', () => {
             console.log("Credits button clicked");
         });
 
         console.log("Title screen added");
     }
-
 }
